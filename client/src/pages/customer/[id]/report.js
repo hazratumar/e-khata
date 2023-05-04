4054503247d132e68fcd08c254dafddf5ebd22a8
mm-umar <h.umar@pk.micromerger.com>
2023-05-05 00:46:32 +0500

    customer wise report

--- client/src/pages/customer/[id]/report.js
+++ client/src/pages/customer/[id]/report.js
@@ -1,6 +1,6 @@
 import {
   Box,
-  Button,
+  Divider,
   Table,
   TableBody,
   TableCell,
@@ -10,20 +10,37 @@ import {
 } from "@mui/material";
 
 const HomePage = () => {
-  const data = [
+  const customerData = [
     { date: "2/5/2023", name: "John", debit: "$100", credit: "$100", balance: "$100" },
     { date: "2/10/2023", name: "Jane", debit: "$100", credit: "$100", balance: "$100" },
     { date: "2/15/2023", name: "Bob", debit: "$100", credit: "$100", balance: "$100" },
   ];
-
   return (
-    <Box sx={{ textAlign: "center", marginTop: "50px" }}>
-      <Typography variant="h5" gutterBottom>
-        Saeed Khan
-      </Typography>
-      <Button variant="contained" sx={{ marginTop: "20px" }}>
-        Click Me Now!
-      </Button>
+    <Box
+      sx={{
+        textAlign: "center",
+        marginTop: "50px",
+        // background: `url(/favicon-16x16.png)`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Box>
+        <Typography variant="h4" gutterBottom>
+          Rahat Shinwari Enterprises
+        </Typography>
+        <Typography variant="h6" gutterBottom>
+          Customer Report
+        </Typography>
+        <Box display="flex" flexDirection="row" justifyContent="space-around" marginY={3}>
+          <Typography>Customer Name: Saeed Khan</Typography>
+          <Typography>Contact: 03444342348</Typography>
+          <Typography>Address: Dubai</Typography>
+          <Typography>Currencies: PKR, DHR, USD, AFG, RMB</Typography>
+        </Box>
+
+        <Divider />
+      </Box>
       <Table>
         <TableHead>
           <TableRow>
@@ -35,7 +52,7 @@ const HomePage = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((item, index) => (
+          {customerData.map((item, index) => (
             <TableRow key={index}>
               <TableCell>{item.date}</TableCell>
               <TableCell>{item.name}</TableCell>
