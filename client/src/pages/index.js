b19259677a5815809c1d41a0fec5aeea9da3aa0e
mm-umar <h.umar@pk.micromerger.com>
2023-06-17 16:41:46 +0500

    profit work done

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,23 +1,38 @@
 import Head from "next/head";
-import { useState, useEffect } from "react";
-import { Box, Container, Grid, CircularProgress } from "@mui/material";
+import { useState } from "react";
+import { Box, Container, Grid, CircularProgress, Typography } from "@mui/material";
 import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
 import { OverviewBudget } from "src/sections/overview/overview-budget";
 import { useGetDashboardDataQuery } from "src/store/services/balanceService";
 import { getDateRange } from "src/utils/generic-functions";
 import { FilterModal } from "src/sections/overview/filterModal";
+import { useGetCurrenciesQuery } from "src/store/services/currencyService";
 
 const Page = () => {
   const [selectedOption, setSelectedOption] = useState(getDateRange(1));
-  const { data, isLoading, refetch } = useGetDashboardDataQuery(selectedOption);
+  const { data, isLoading } = useGetDashboardDataQuery(selectedOption);
+  const { data: currenciesData } = useGetCurrenciesQuery({ page: 0, rowsPerPage: 100 });
+
+  const currencyAbbreviations = currenciesData?.currencies.map((currency) => currency.abbreviation);
+
+  const sumOfDebits = calculateSum(data?.sumOfDebits);
+  const sumOfCredits = calculateSum(data?.sumOfCredits);
 
   const filterDashboard = (data) => {
     setSelectedOption(data);
   };
 
-  useEffect(() => {
-    refetch();
-  }, [selectedOption]);
+  function calculateSum(amounts) {
+    if (!amounts) {
+      return 0;
+    }
+    return currenciesData?.currencies.reduce((sum, currency) => {
+      if (currencyAbbreviations.includes(currency.abbreviation) && amounts[currency.abbreviation]) {
+        return sum + currency.rate * amounts[currency.abbreviation];
+      }
+      return sum;
+    }, 0);
+  }
 
   return (
     <>
@@ -26,7 +41,7 @@ const Page = () => {
       </Head>
       <Box component="main">
         <Container maxWidth="xl">
-          <Grid container spacing={3} my={2} justifyContent="flex-end">
+          <Grid container spacing={3} my={1} justifyContent="flex-end">
             <FilterModal filterDashboard={filterDashboard} />
           </Grid>
           <Grid container spacing={3}>
@@ -48,6 +63,13 @@ const Page = () => {
                     <OverviewBudget type="Debit" abbreviation={abbreviation} value={amount} />
                   </Grid>
                 ))}
+                <Grid key={1} item xs={12} sm={12} md={12} lg={12} pb={3}>
+                  <Box bgcolor="#f5f5f5" p={2} borderRadius={4}>
+                    <Typography variant="h5" component="h2" align="center">
+                      Net Profit in Dirham: {sumOfDebits - sumOfCredits}
+                    </Typography>
+                  </Box>
+                </Grid>
               </>
             )}
           </Grid>
